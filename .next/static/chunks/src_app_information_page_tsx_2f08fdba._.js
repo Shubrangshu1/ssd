547349bf60b4bf4f905fd1ec30d0c84e46c5a48a
Shubrangshu Giri <shubrangshu1@gmail.com>
2025-06-18 07:49:17 +0000

    theme colour should be white and light blue baby blue make some effects

---
+++ .next/static/chunks/src_app_information_page_tsx_2f08fdba._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_information_page_tsx_2f08fdba._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_information_page_tsx_2f08fdba._.js",
+  "chunks": [
+    "static/chunks/_02ca5eab._.js"
+  ],
+  "source": "dynamic"
+});
---
+++ .next/static/chunks/src_components_UpiPayment_tsx_1c3699d5._.js
@@ -0,0 +1,16 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push(["static/chunks/src_components_UpiPayment_tsx_1c3699d5._.js", {
+
+"[project]/src/components/UpiPayment.tsx [app-client] (ecmascript, next/dynamic entry, async loader)": ((__turbopack_context__) => {
+
+var { g: global, __dirname } = __turbopack_context__;
+{
+__turbopack_context__.v((parentImport) => {
+    return Promise.all([
+  "static/chunks/_29a5e0ff._.js",
+  "static/chunks/src_components_UpiPayment_tsx_ab88b5ef._.js"
+].map((chunk) => __turbopack_context__.l(chunk))).then(() => {
+        return parentImport("[project]/src/components/UpiPayment.tsx [app-client] (ecmascript, next/dynamic entry)");
+    });
+});
+}}),
+}]);
